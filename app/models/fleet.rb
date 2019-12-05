c8bc27bce0da49fe48ebad213a979d8ccdb2d1aa
dan0505 <qiaoer0505@gmail.com>
2019-12-05 16:28:56 +1100

    add admin

--- app/models/fleet.rb
+++ app/models/fleet.rb
@@ -1,5 +1,5 @@
 class Fleet < ApplicationRecord
-    # the actuale plane which reference from plane model for details (rows, seats/row etc)
-    belongs_to :plane
-    has_many :fleet, class_name: "Flight", foreign_key: "fleet_id"
+  # the actuale plane which reference from plane model for details (rows, seats/row etc)
+  belongs_to :plane
+  has_many :fleet, class_name: "Flight", foreign_key: "fleet_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+
+  # enum admin: [:admin, :user]
 end
