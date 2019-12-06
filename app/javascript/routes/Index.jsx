790f2553782a361402f82f27903a8f8bb8d6af52
dan0505 <qiaoer0505@gmail.com>
2019-12-06 17:24:38 +1100

    add flight page to route

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 import Flights from "../components/Flights";
+import Flight from "../components/Flight";
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/flights" exact component={Flights} />
+      <Route path="/flights/:id" exact component={Flight} />
     </Switch>
   </Router>
 );
