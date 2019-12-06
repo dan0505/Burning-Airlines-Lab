094e2dff0c471934e9fa16dd869e9b356ff5de21
dan0505 <qiaoer0505@gmail.com>
2019-12-06 16:36:58 +1100

    added flights overview page

--- app/javascript/components/Flights.jsx
+++ app/javascript/components/Flights.jsx
@@ -2,40 +2,92 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class Flights extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            flights: []
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      flights: []
+    };
 
-    }
+  }
 
-
-
-componentDidMount() {
+  componentDidMount() {
     const url = "/api/v1/flights";
     fetch(url)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(" Shitty Network, Sorry! ")
-        })
-        .then(response => this.setState({ flights = response }))
-        .catch(() => this.props.history.push("/"));
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error(" Shitty Network, Sorry! ")
+      })
+      .then(response => this.setState({ flights: response }))
+      .catch(() => this.props.history.push("/"));
+  }
 
+  render() {
+    const { flights } = this.state;
+    const allFlights = flights.map((flight, index) => (
+      <div key={index} className="col-md-6 col-lg-4">
+        <div className="card mb-4">
+          {/* <img
+            src={recipe.image}
+            className="card-img-top"
+            alt={`${recipe.name} image`}
+          /> */}
+          <div className="card-body">
+            <h5 className="card-title">{flight.id}</h5>
+            <Link to={`/flight/${flight.id}`} className="btn custom-button">
+              Goto Flight
+            </Link>
+          </div>
+        </div>
+      </div>
+    ));
+    const noFlight = (
+      <div className="vw-100 vh-50 d-flex align-items-center justify-content-center">
+        <h4>
+          No Flight available. Why not <Link to="/new_flight">create one and flight it yourself</Link>
+        </h4>
+      </div>
+    );
+
+    var sectionStyle = {
+      backgroundImage: "url(https://images.unsplash.com/photo-1484848016494-33ad4fc7feef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80)",
+      backgroundSize: "cover",
+      overflow: 'hidden',
+    }
+    var jumbotronStyle = {
+      backgroundColor: 'rgba(255, 255, 255, 0.7)'
+    }
+
+    return (
+      <>
+        <section className="jumbotron jumbotron-fluid text-center" style={sectionStyle}>
+          <div className="container py-5 rounded" style={jumbotronStyle}>
+            <h1 className="display-4">Flights for every destinations</h1>
+            <p className="lead text-muted">
+              We only fly the hotest people to the hotest destinations
+            </p>
+          </div>
+        </section>
+        <div className="py-5">
+          <main className="container">
+            <div className="row">
+              {flights.length > 0 ? allFlights : noFlight}
+            </div>
+            <div className="text-right mb-3">
+              <Link to="/flight" className="btn custom-button">
+                Can you fly?
+              </Link>
+            </div>
+            <Link to="/" className="btn btn-link">
+              Home
+            </Link>
+          </main>
+        </div>
+      </>
+    );
+  }
 }
-
-render() {
-    return  flights
-}
-
-
-
-
-}
-
-
 export default Flights;
 
 
