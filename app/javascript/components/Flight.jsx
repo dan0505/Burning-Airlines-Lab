8881d8b5eeb953883ccd948f86fee3dbc1e8babb
dan0505 <qiaoer0505@gmail.com>
2019-12-06 17:24:46 +1100

    add flight component

---
+++ app/javascript/components/Flight.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Flight extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { flight: "" };
+
+    this.addHtmlEntities = this.addHtmlEntities.bind(this);
+  }
+
+  componentDidMount() {
+    const {
+      match: {
+        params: { id }
+      }
+    } = this.props;
+
+    const url = `/api/v1/flights/${id}`;
+
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(response => this.setState({ flight: response }))
+      .catch(() => this.props.history.push("/flights"));
+  }
+
+  addHtmlEntities(str) {
+    return String(str)
+      .replace(/&lt;/g, "<")
+      .replace(/&gt;/g, ">");
+  }
+
+  render() {
+
+    const { flight } = this.state;
+    console.log(flight);
+    let ingredientList = "No ingredients available";
+    // const total_seat = Number(flight.plane_row) * Number(flight.plane_col)
+    // const booked_seats = Number(flight.booked_seats.length)
+
+    // if (flight..length > 0) {
+    //   ingredientList = recipe.ingredients
+    //     .split(",")
+    //     .map((ingredient, index) => (
+    //       <li key={index} className="list-group-item">
+    //         {ingredient}
+    //       </li>
+    //     ));
+    // }
+    const recipeInstruction = this.addHtmlEntities(flight.plane_model);
+
+    return (
+      <div className="">
+        <div className="hero position-relative d-flex align-items-center justify-content-center">
+          <img
+            src="https://home.bt.com/images/passenger-plane-landed-in-hudson-river-136395463010202601-150114153840.jpg"
+            alt={`${flight.id} image`}
+            className="img-fluid position-absolute"
+          />
+          <div className="overlay bg-dark position-absolute" />
+          <h1 className="display-4 position-relative text-white">
+            {recipe.name}
+          </h1>
+        </div>
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-sm-12 col-lg-3">
+              <ul className="list-group">
+                <h5 className="mb-2">Plane Layout</h5>
+                {ingredientList}
+              </ul>
+            </div>
+            <div className="col-sm-12 col-lg-7">
+              <h5 className="mb-2">Preparation Instructions</h5>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: `${recipeInstruction}`
+                }}
+              />
+            </div>
+            <div className="col-sm-12 col-lg-2">
+              <button type="button" className="btn btn-danger">
+                Delete Recipe
+              </button>
+            </div>
+          </div>
+          <Link to="/flights" className="btn btn-link">
+            Back to flights
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default Flight;
